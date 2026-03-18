8cd6c667c7485d2e9449ccb8dbe058617034bbf8
otllan <ottlan0623@skola.goteborg.se>
2026-03-18 14:25:21 +0100

    Update jORP.py

--- jORP.py
+++ jORP.py
@@ -1,21 +1,16 @@
 import time
 import os
 import socket
-
 cooldown = "10"
 software = "C:\Users\YourUsername\AppData\Local\Google\Chrome\User Data"
-
 cooldown_seconds = 10
-
 for i in range(cooldown_seconds, 0, -1):
     print(f"cooldown = {i}")
     time.sleep(1)  
 print(cooldown)
 print("Process complete!")
-
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
-
 print("Your Computer Name is:", hostname)
 print("Your Computer IP Address is:", IPAddr)
 print("deleting os")
